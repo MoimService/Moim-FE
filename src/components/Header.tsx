4b228343eb4cc244e71cec78abf2df5e77e1ab6e
dbswl701 <dbswl701@ajou.ac.kr>
2025-02-13 17:13:43 +0900

    feat(DEVING-15): GNB 로그인 후 구현

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,28 +1,48 @@
-import Logo from '@/assets/logo.svg';
+import Logo from '@/assets/icon/logo.svg';
+import Profile from '@/assets/icon/profile.svg';
 import Image from 'next/image';
 import Link from 'next/link';
 
-const Header = () => {
+const BeforeLogin = () => {
   return (
-    <header className="h-20 bg-BG flex items-center">
-      <div className="max-w-[1340px] w-full flex justify-between item-center mx-auto">
+    <nav className="ml-auto flex">
+      <Link
+        href="/login"
+        className="text-head3 flex h-[54px] w-[91px] items-center justify-center text-Cgray500"
+      >
+        로그인
+      </Link>
+      <Link
+        href="/signup"
+        className="text-head3 flex  h-[54px] w-[91px] items-center justify-center text-Cgray500"
+      >
+        회원가입
+      </Link>
+    </nav>
+  );
+};
+
+const AfterLogin = () => {
+  return (
+    <nav className="ml-auto flex">
+      <div className="flex items-center">
+        <Image src={Profile} width={40} height={40} alt="logo" />
+        <span className="typo-head3 m-auto w-[77px] text-center text-Cgray500">
+          김밥식
+        </span>
+      </div>
+    </nav>
+  );
+};
+
+const Header = ({ isLogIn = false }) => {
+  return (
+    <header className="flex h-20 items-center bg-BG">
+      <div className="item-center mx-auto flex w-full max-w-[1340px] items-center justify-between">
         <div className="flex-shrink-0">
           <Image src={Logo} width={40} height={40} alt="logo" />
         </div>
-        <nav className="flex ml-auto">
-          <Link
-            href="/login"
-            className="w-[91px] h-[54px] text-head3 text-Cgray500 flex items-center justify-center"
-          >
-            로그인
-          </Link>
-          <Link
-            href="/signup"
-            className="w-[91px] h-[54px] text-head3 text-Cgray500 flex items-center justify-center"
-          >
-            회원가입
-          </Link>
-        </nav>
+        {!isLogIn ? <AfterLogin /> : <BeforeLogin />}
       </div>
     </header>
   );
