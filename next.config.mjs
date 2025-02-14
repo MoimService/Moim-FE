d5ae48cc484d4fdd8e1df1bd14d2fb859ec1961b
lee1nna <dlgkssk0209@gmail.com>
2025-02-14 13:45:40 +0900

    style(DEVING-16): Card 컴포넌트 스타일 커스텀

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,9 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  images: {
+    // TODO: 서버 이미지 도메인으로 수정
+    domains: ['helpx.adobe.com'],
+  },
+};
 
 export default nextConfig;
--- src/components/ui/Card.tsx
+++ src/components/ui/Card.tsx
@@ -1,32 +1,106 @@
+'use client';
+
 import { cn } from '@/util/cn';
+import { cva } from 'class-variance-authority';
+import Image from 'next/image';
 import * as React from 'react';
 
-const Card = React.forwardRef<
-  HTMLDivElement,
-  React.HTMLAttributes<HTMLDivElement>
->(({ className, ...props }, ref) => (
-  <div
-    ref={ref}
-    className={cn(
-      'rounded-lg border border-neutral-200 bg-white text-neutral-950 shadow-sm dark:border-neutral-800 dark:bg-neutral-950 dark:text-neutral-50',
-      className,
-    )}
-    {...props}
-  />
-));
+type CardType = 'vertical' | 'horizon';
+
+interface CardContextValue {
+  type?: CardType;
+  imgUrl: string;
+}
+
+const CardContext = React.createContext<CardContextValue>({
+  type: 'vertical',
+  imgUrl: '',
+});
+
+interface CardProps extends React.HTMLAttributes<HTMLDivElement> {
+  type?: CardType;
+  imgUrl: string;
+}
+
+const useCardContext = () => {
+  const context = React.useContext(CardContext);
+  return context;
+};
+
+const cardVariants = cva('flex bg-BG p-4', {
+  variants: {
+    type: {
+      vertical: ['flex-col w-[466px]'],
+      horizon: ['flex-row w-[1340px]'],
+    },
+  },
+  defaultVariants: {
+    type: 'vertical',
+  },
+});
+
+const Card = React.forwardRef<HTMLDivElement, CardProps>(
+  ({ className, type = 'vertical', imgUrl = '', ...props }, ref) => {
+    return (
+      <CardContext.Provider value={{ type, imgUrl }}>
+        <div
+          ref={ref}
+          className={cn(cardVariants({ type }), className)}
+          {...props}
+        />
+      </CardContext.Provider>
+    );
+  },
+);
 Card.displayName = 'Card';
 
-const CardHeader = React.forwardRef<
+const cardThumbnailVariants = cva(
+  'flex flex-col space-y-1.5 rounded-[20px] object-cover',
+  {
+    variants: {
+      type: {
+        vertical: ['mt-[12px] w-[432px] h-[252px]'],
+        horizon: ['ml-[24px] w-[252px] h-[208px]'],
+      },
+    },
+    defaultVariants: {
+      type: 'vertical',
+    },
+  },
+);
+
+const CardThumbnail = ({
+  className,
+  ...props
+}: Omit<React.HTMLAttributes<HTMLImageElement>, 'children'> & {
+  className?: string;
+}) => {
+  const { type, imgUrl } = useCardContext();
+
+  return (
+    <Image
+      src={imgUrl}
+      alt="card_img"
+      width={434}
+      height={252}
+      className={cn(cardThumbnailVariants({ type }), className)}
+      {...props}
+    />
+  );
+};
+CardThumbnail.displayName = 'CardThumbnail';
+
+const CardCategory = React.forwardRef<
   HTMLDivElement,
   React.HTMLAttributes<HTMLDivElement>
 >(({ className, ...props }, ref) => (
   <div
     ref={ref}
-    className={cn('flex flex-col space-y-1.5 p-6', className)}
+    className={cn('type-botton1 mt-[10px] text-sm text-main', className)}
     {...props}
   />
 ));
-CardHeader.displayName = 'CardHeader';
+CardCategory.displayName = 'CardCategory';
 
 const CardTitle = React.forwardRef<
   HTMLDivElement,
@@ -35,7 +109,7 @@ const CardTitle = React.forwardRef<
   <div
     ref={ref}
     className={cn(
-      'text-2xl font-semibold leading-none tracking-tight',
+      'typo-head2 mt-4 font-semibold leading-none tracking-tight text-Cgray800',
       className,
     )}
     {...props}
@@ -49,18 +123,40 @@ const CardDescription = React.forwardRef<
 >(({ className, ...props }, ref) => (
   <div
     ref={ref}
-    className={cn('text-sm text-neutral-500 dark:text-neutral-400', className)}
+    className={cn('type-botton1 mt-3 text-sm text-Cgray500', className)}
     {...props}
   />
 ));
 CardDescription.displayName = 'CardDescription';
 
+const cardContentVariants = cva('flex flex-col', {
+  variants: {
+    type: {
+      vertical: ['mt-[12px]'],
+      horizon: ['ml-[24px] w-[690px]'],
+    },
+  },
+  defaultVariants: {
+    type: 'vertical',
+  },
+});
+
 const CardContent = React.forwardRef<
   HTMLDivElement,
   React.HTMLAttributes<HTMLDivElement>
->(({ className, ...props }, ref) => (
-  <div ref={ref} className={cn('p-6 pt-0', className)} {...props} />
-));
+>(({ className, children, ...props }, ref) => {
+  const { type } = useCardContext();
+
+  return (
+    <div
+      ref={ref}
+      className={cn(cardContentVariants({ type }), className)}
+      {...props}
+    >
+      {children}
+    </div>
+  );
+});
 CardContent.displayName = 'CardContent';
 
 const CardFooter = React.forwardRef<
@@ -77,7 +173,8 @@ CardFooter.displayName = 'CardFooter';
 
 export {
   Card,
-  CardHeader,
+  CardCategory,
+  CardThumbnail,
   CardFooter,
   CardTitle,
   CardDescription,
