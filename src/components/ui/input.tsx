198bd5a3a3da19541d22f2d662ea44113d42f7c5
dbswl701 <dbswl701@ajou.ac.kr>
2025-02-14 17:14:41 +0900

    refactor(DEVING-34): 스타일 클래스 조합 cn에서 cva로 리펙토링

--- src/components/ui/input.tsx
+++ src/components/ui/input.tsx
@@ -1,45 +1,56 @@
 import { cn } from '@/util/cn';
+import { VariantProps, cva } from 'class-variance-authority';
 import * as React from 'react';
 
-interface IInputProps extends React.ComponentProps<'input'> {
-  isValid?: boolean;
+interface IInputProps
+  extends React.InputHTMLAttributes<HTMLInputElement>,
+    VariantProps<typeof inputVariants> {
   errorMessage?: string;
-  inputSize?: 's' | 'l';
 }
 
+const inputVariants = cva(
+  'box-border flex w-full rounded-md border-input bg-Cgray200 px-3 px-[16px] py-1 py-[14px] text-base text-Cgray700 caret-Cgray500 shadow-sm transition-colors file:border-0 file:bg-transparent file:text-sm file:font-medium file:text-foreground placeholder:text-Cgray400 placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:cursor-not-allowed disabled:bg-disable disabled:text-disable_text md:text-sm',
+  {
+    variants: {
+      isValid: {
+        true: 'border border-main',
+        false: 'border border-warning text-warning caret-warning',
+      },
+      inputSize: {
+        s: 'typo-button2 h-[36px]',
+        l: 'typo-button1 h-[50px] py-[10px]',
+      },
+    },
+    defaultVariants: {
+      inputSize: 'l',
+    },
+  },
+);
+
+const errorTextVariants = cva('px-[10px] text-warning', {
+  variants: {
+    inputSize: {
+      s: 'typo-caption2 mt-[8px]',
+      l: 'typo-caption1 mt-[10px]',
+    },
+    defaultVariants: {
+      inputSize: 'l',
+    },
+  },
+});
+
 const Input = React.forwardRef<HTMLInputElement, IInputProps>(
-  (
-    { className, type, isValid, errorMessage, inputSize = 'l', ...props },
-    ref,
-  ) => {
+  ({ className, type, isValid, errorMessage, inputSize, ...props }, ref) => {
     return (
       <div className="w-full">
         <input
           type={type}
-          className={cn(
-            'flex w-full rounded-md border-input bg-transparent px-3 py-1 text-base shadow-sm transition-colors file:border-0 file:bg-transparent file:text-sm file:font-medium file:text-foreground placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring md:text-sm',
-            'typo-button1 box-border h-[50px] bg-Cgray200 px-[16px] py-[14px] text-Cgray700 caret-Cgray500 placeholder:text-Cgray400',
-            'disabled:cursor-not-allowed disabled:bg-disable disabled:text-disable_text',
-            isValid === undefined
-              ? ''
-              : isValid
-                ? 'border border-main'
-                : 'border border-warning text-warning caret-warning',
-            inputSize === 's' ? 'typo-button2 h-[36px]' : 'py-[10px]',
-            className,
-          )}
+          className={cn(inputVariants({ isValid, inputSize, className }))}
           ref={ref}
           {...props}
         />
         {errorMessage && (
-          <p
-            className={cn(
-              ' px-[10px] text-warning',
-              inputSize === 's'
-                ? 'typo-caption2 mt-[8px]'
-                : 'typo-caption1 mt-[10px]',
-            )}
-          >
+          <p className={cn(errorTextVariants({ inputSize, className }))}>
             {errorMessage}
           </p>
         )}
@@ -47,6 +58,7 @@ const Input = React.forwardRef<HTMLInputElement, IInputProps>(
     );
   },
 );
+
 Input.displayName = 'Input';
 
 export { Input };
