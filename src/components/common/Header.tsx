b3b94f2f812172a93b4935e87c6f3750dfe60922
dbswl701 <dbswl701@ajou.ac.kr>
2025-02-18 14:46:07 +0900

    feat(DEVING-15): 메뉴 열고 닫는 기능 추가

--- src/components/common/Header.tsx
+++ src/components/common/Header.tsx
@@ -4,6 +4,7 @@ import Logo from '@/assets/icon/logo.svg';
 import { Menu } from 'lucide-react';
 import Image from 'next/image';
 import Link from 'next/link';
+import { useState } from 'react';
 
 import Profile from '../../assets/icon/profile.svg';
 import Dropdown from './Dropdown';
@@ -45,10 +46,16 @@ const AfterLogin = () => {
 const MobileBeforeLogin = () => {
   return (
     <div className="py-[24px]">
-      <Link className="typo-head3 p-[16px] text-Cgray500" href="/login">
+      <Link
+        className="typo-head3 p-[16px] text-Cgray500 hover:text-Cgray700"
+        href="/login"
+      >
         로그인
       </Link>
-      <Link className="typo-head3 p-[16px] text-main" href="/signup">
+      <Link
+        className="typo-head3 p-[16px] text-main hover:text-[#5C73FF]"
+        href="/signup"
+      >
         회원가입
       </Link>
     </div>
@@ -59,7 +66,10 @@ const MobileAfterLogin = () => {
   return (
     <div className="flex flex-col py-[24px]">
       <div className="flex justify-between">
-        <Link className="typo-head3 p-[16px] text-Cgray500" href="/login">
+        <Link
+          className="typo-head3 p-[16px] text-Cgray500 hover:opacity-5"
+          href="/login"
+        >
           로그아웃
         </Link>
         <div className="flex">
@@ -79,36 +89,76 @@ const MobileAfterLogin = () => {
   );
 };
 
+const NavLinks = ({ isMobile }: { isMobile?: boolean }) => {
+  return (
+    <ul
+      className={`${!isMobile ? 'hidden items-center text-Cgray700 lg:flex' : 'text-Cgray400'}`}
+    >
+      <li className="typo-head4 p-[16px]">
+        <Link
+          href="/Mogakco"
+          className={`${isMobile ? 'hover:text-Cgray500' : 'hover:text-white'}`}
+        >
+          모각코
+        </Link>
+      </li>
+      <li className="typo-head4 p-[16px]">
+        <Link
+          href="/study"
+          className={`${isMobile ? 'hover:text-Cgray500' : 'hover:text-white'}`}
+        >
+          스터디
+        </Link>
+      </li>
+      <li className="typo-head4 p-[16px]">
+        <Link
+          href="/side-project"
+          className={`${isMobile ? 'hover:text-Cgray500' : 'hover:text-white'}`}
+        >
+          사이드 프로젝트
+        </Link>
+      </li>
+      <li className="typo-head4 p-[16px]">
+        <Link
+          href="/hobby"
+          className={`${isMobile ? 'hover:text-Cgray500' : 'hover:text-white'}`}
+        >
+          취미
+        </Link>
+      </li>
+    </ul>
+  );
+};
+
 const Header = ({ isLogIn = false }) => {
+  const [isOpen, setIsOpen] = useState(false);
   return (
     <div>
-      <header className="flex h-20 items-center bg-BG px-[24px] lg:bg-main">
+      {/* desktop */}
+      <header
+        className={`flex h-20 items-center bg-BG px-[24px] lg:bg-main ${!isOpen && 'bg-main'}`}
+      >
         <div className="item-center mx-auto flex w-full max-w-[1340px] items-center justify-between">
-          <div className="mr-[40px] flex-shrink-0">
+          <Link href="/" className="mr-[40px] flex-shrink-0">
             <Image src={Logo} width={40} height={40} alt="logo" />
-          </div>
-          <div className="hidden items-center lg:flex">
-            <ul className="flex items-center text-Cgray700">
-              <li className="typo-head4 p-[16px]">모각코</li>
-              <li className="typo-head4 p-[16px]">스터디</li>
-              <li className="typo-head4 p-[16px]">사이드 프로젝트</li>
-              <li className="typo-head4 p-[16px]">취미</li>
-            </ul>
-          </div>
+          </Link>
+          <NavLinks />
           {!isLogIn ? <BeforeLogin /> : <AfterLogin />}
-          <div className="text-white lg:hidden">
-            <Menu />
-          </div>
+          <Menu
+            className="text-white lg:hidden"
+            onClick={() => setIsOpen((prev) => !prev)}
+          />
         </div>
       </header>
-      <div className="absolute h-screen w-screen bg-BG px-[24px]  lg:hidden">
+
+      {/* mobile */}
+      <div
+        className={`absolute right-0 h-screen w-screen transform bg-BG px-[24px] transition-transform duration-300 ease-in-out ${
+          isOpen ? 'translate-x-0' : 'translate-x-full'
+        } lg:hidden`}
+      >
         {!isLogIn ? <MobileBeforeLogin /> : <MobileAfterLogin />}
-        <ul className="text-Cgray400">
-          <li className="typo-head3 p-[16px]">모각코</li>
-          <li className="typo-head3 p-[16px]">스터디</li>
-          <li className="typo-head3 p-[16px]">사이드 프로젝트</li>
-          <li className="typo-head3 p-[16px]">취미</li>
-        </ul>
+        <NavLinks isMobile />
       </div>
     </div>
   );
